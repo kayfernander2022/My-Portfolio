8b6cd08a75f875bb9c74ae3c41fcfd824f231c9a
kayfernander2022 <kayfernander@hotmail.com>
2023-05-03 11:53:33 -0400

    update heading color for better contrast

--- src/components/footer/socials.tsx
+++ src/components/footer/socials.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faGithub,
-    faTumblr,
-    faLinkedin,
-} from '@fortawesome/free-brands-svg-icons'
+    faGithub,faTumblr,faLinkedin,} from '@fortawesome/free-brands-svg-icons'
 import Social from './../../models/social'
 
 export const Socials: React.FC<{ list: Social[] }> = props => {
